302232c8ee245c29a988c1761b893830ec45fa6b
Mana Pecar <mana.pecar@gmail.com>
2023-09-15 21:33:32 +0200

    clean code

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import Tables from "./components/Tables";
+import Form from "./components/Form";
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Inventory Manager</h1>
+        <div>
+        <Tables />
+        </div>
+        <div>
+        <Form />
+        </div>
       </header>
     </div>
   );
